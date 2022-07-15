1d3e40d5a3f4eaac9ec494b8aeda4d3a9082c541
Ruslan.Belous <r.bilous@viseven.com>
2022-07-15 13:01:26 +0300

    deploy

---
+++ precache-manifest.934f5c863ae2030d418dd13935f30846.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808aa0fd5750a0b87542",
+    "url": "/v-films-api/css/app.7bd7edbe.css"
+  },
+  {
+    "revision": "cab391fbeb1d9d36f561",
+    "url": "/v-films-api/css/chunk-vendors.34378d7f.css"
+  },
+  {
+    "revision": "39bfea5e86f5f41c9d2896dbbed6791b",
+    "url": "/v-films-api/fonts/MaterialIcons-Regular.39bfea5e.woff"
+  },
+  {
+    "revision": "ae8b1248595e70a828b880b9c56963da",
+    "url": "/v-films-api/fonts/MaterialIcons-Regular.ae8b1248.eot"
+  },
+  {
+    "revision": "b073f5972d9c4cc1b8ae8e071e441376",
+    "url": "/v-films-api/fonts/MaterialIcons-Regular.b073f597.woff2"
+  },
+  {
+    "revision": "b9968c1adf542baec8d2b298d3ff437c",
+    "url": "/v-films-api/fonts/MaterialIcons-Regular.b9968c1a.ttf"
+  },
+  {
+    "revision": "59798f3710d3a0f535cc82ec863cad2a",
+    "url": "/v-films-api/img/empty.59798f37.jpeg"
+  },
+  {
+    "revision": "3b3e691ad15b6169b1d32f36f618cd60",
+    "url": "/v-films-api/index.html"
+  },
+  {
+    "revision": "808aa0fd5750a0b87542",
+    "url": "/v-films-api/js/app.7c7af8f3.js"
+  },
+  {
+    "revision": "cab391fbeb1d9d36f561",
+    "url": "/v-films-api/js/chunk-vendors.13cf7d4a.js"
+  },
+  {
+    "revision": "62b341b3f462941dc858fe3ceb709ea6",
+    "url": "/v-films-api/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/v-films-api/robots.txt"
+  }
+]);
